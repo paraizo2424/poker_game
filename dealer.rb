6454de3b56e1ae7c200228349ff8fa5db578015c
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2020-07-12 22:09:43 +0900

    dealerのdecksを簡略化

--- dealer.rb
+++ dealer.rb
@@ -2,27 +2,27 @@ require "./deck"
 
 class Dealer
   def initialize
-    @deck = Deck.new
+    @deck = Deck.new.decks
   end
 
   def shuffle
-    @deck.decks.shuffle!
+    @deck.shuffle!
   end
 
   def deal(num)
-    deal_kards = @deck.decks.first(num)
-    @deck.decks.slice!(0..num-1)
+    deal_kards = @deck.first(num)
+    @deck.slice!(0..num-1)
     deal_kards
   end
 
   def show(num)
-    puts @deck.decks.first(num)
+    puts @deck.first(num)
   end
 
   def change(hands)
     hands.length.times do |num|
       if hands[num].nil?
-        hands[num] = @deck.decks.shift
+        hands[num] = @deck.shift
       end
     end
     hands
