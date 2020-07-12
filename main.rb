482856379f15fda46f0751c2f4d773d23ba96dbe
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2020-07-12 20:52:12 +0900

    mainを微修正

--- main.rb
+++ main.rb
@@ -1,15 +1,19 @@
 require "./dealer"
 require "./player"
 
+puts "＜ゲーム開始＞"
+
+# ディーラーがトランプ一組を持って現れる
 dealer = Dealer.new
 dealer.shuffle
-dealer.show(10)
 puts "--------"
 
+# プレイヤーが卓に座る
 player = Player.new(dealer)
+
+# プレイヤーの手札を表示する
 player.show
 
 puts "--------"
-dealer.show(10)
 
-puts "--------"
+puts "＜ゲーム終了＞"
